1e9b76725f0e9915276ca1ff4de723f7754a49f4
Vinicius Coque <vcoque@gmail.com>
2015-11-17 17:41:55 -0200

    use inflate method instead of an ItemView object

--- app/src/main/java/br/com/locaweb/jacatv/adapter/ShowAdapter.java
+++ app/src/main/java/br/com/locaweb/jacatv/adapter/ShowAdapter.java
@@ -5,19 +5,16 @@ import android.view.ViewGroup;
 
 import org.androidannotations.annotations.EBean;
 
+import br.com.locaweb.jacatv.fragment.ShowDetailFragment_;
 import br.com.locaweb.jacatv.model.Show;
-import br.com.locaweb.jacatv.view.ShowDetailView;
-import br.com.locaweb.jacatv.view.ShowDetailView_;
 
 @EBean
 public class ShowAdapter extends AABaseAdapter<Show> {
     @Override
     public View getView(int position, View convertView, ViewGroup viewGroup) {
         if (convertView == null) {
-            convertView = ShowDetailView_.build(context);
+            convertView = View.inflate(context, android.R.layout.simple_list_item_1, null);
         }
-
-        ((ShowDetailView) convertView).bind(getItem(position));
 
         return convertView;
     }
--- app/src/main/java/br/com/locaweb/jacatv/view/ShowDetailView.java
+++
@@ -1,7 +0,0 @@
-package br.com.locaweb.jacatv.view;
-
-/**
- * Created by treinamentomobile on 11/17/15.
- */
-public class ShowDetailView {
-}
